42eaca56958bf94a9ae13e953f3a8f8cc2d8bb42
Tim Lucas <t@toolmantim.com>
2016-02-17 16:59:22 +1100

    Fix URL

--- trigger_async.sh
+++ trigger_async.sh
@@ -3,7 +3,7 @@
 set -eu
 
 curl \
-  "https://api.buildkite.com/v2/organizations/${TRIGGER_ORG_SLUG}/pipelines/${TRIGGER_PIPELINE_SLUG}" \
+  "https://api.buildkite.com/v2/organizations/${TRIGGER_ORG_SLUG}/pipelines/${TRIGGER_PIPELINE_SLUG}/builds" \
   -X POST \
   --fail \
   -H "Authorization: Bearer ${TRIGGER_API_ACCESS_TOKEN}" \
